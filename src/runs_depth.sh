adf22a01acf2f42fb9539c613aaa920947654f1b
Nicolas Emiliani <n.r.emiliani@gmail.com>
2014-07-30 21:08:47 -0300

    adds depth and learning rate test scripts

---
+++ src/runs_depth.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 500 -l 0.7 -m 1
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 500 -l 0.7 -m 2
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 500 -l 0.7 -m 3
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 500 -l 0.7 -m 4
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 500 -l 0.7 -m 5
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 500 -l 0.7 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 500 -l 0.7 -m 7
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 500 -l 0.7 -m 8
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 500 -l 0.7 -m 9
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 500 -l 0.7 -m 10
---
+++ src/runs_learn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 0.1 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 0.2 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 0.3 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 0.4 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 0.5 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 0.6 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 0.7 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 0.8 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 0.9 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 1.0 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 1.1 -m 6
+python train.py --printg -q -o graphs/ -s classifiers/ -r data/target_center.pickle -d data/data_center.pickle -t 0.7 -e 1500 -l 1.2 -m 6
